4f24347876497c9694cba205589401e3a494da32
Swarzstein <zorrillagja@gmail.com>
2023-10-03 15:15:16 -0400

    required links set and basic style

--- app/javascript/components/NavPanel.js
+++ app/javascript/components/NavPanel.js
@@ -9,9 +9,11 @@ export default function NavPanel() {
       </div>
       <nav className="nav">
         <Link to="/" className="nav-link">Home</Link>
-        <Link to="/sign_up" className="nav-link">Sign Up</Link>
-        <Link to="/sign_in" className="nav-link">Sign In</Link>
+        <Link to="/trip_plans" className="nav-link">Trip Plans</Link>
+        <Link to="/reserve" className="nav-link">Reserve</Link>
+        <Link to="/my_reservations" className="nav-link">My Reservations</Link>
         <Link to="/add_trip" className="nav-link">Add Trip Plan</Link>
+        <Link to="/delete_trip" className="nav-link">Delete Trip Plan</Link>
       </nav>
     </div>
   );
